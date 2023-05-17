d57fc2bcf2e86572b18ff80f434f172a6054a1fa
unknown <ricanteq@gmail.com>
2023-05-17 19:57:24 +0100

    second commit

--- src/userform.tsx
+++ src/userform.tsx
@@ -1,75 +1,16 @@
-/*import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
-import * as Yup from 'yup';
 
-
-interface UserFormValues {
-  firstName: string;
-  lastName: string;
-  email: string;
-}
-
-const UserForm: React.FC = () => {
-  const initialValues: UserFormValues = {
-    firstName: '',
-    lastName: '',
-    email: '',
-  };
-
-  const validationSchema = Yup.object({
-    firstName: Yup.string().required('First Name is required'),
-    lastName: Yup.string().required('Last Name is required'),
-    email: Yup.string().email('Invalid email address').required('Email is required'),
-  });
-
-  const handleSubmit = (values: UserFormValues) => {
-    console.log(values);
-    // You can perform further actions with the form values here
-  };
-
-  return (
-    <div>
-      <h1>User Form</h1>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={validationSchema}
-        onSubmit={handleSubmit}
-      >
-        <Form>
-          <div>
-            <label htmlFor="firstName">First Name</label>
-            <Field type="text" id="firstName" name="firstName" />
-            <ErrorMessage name="firstName" component="div" />
-          </div>
-          <div>
-            <label htmlFor="lastName">Last Name</label>
-            <Field type="text" id="lastName" name="lastName" />
-            <ErrorMessage name="lastName" component="div" />
-          </div>
-          <div>
-            <label htmlFor="email">Email</label>
-            <Field type="email" id="email" name="email" />
-            <ErrorMessage name="email" component="div" />
-          </div>
-          <button type="submit">Submit</button>
-        </Form>
-      </Formik>
-    </div>
-  );
-};
-
-export default UserForm;*/
 
 import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
-
+import axios from 'axios';
 import './userform.css'; // Import custom CSS file for additional styling
 
 interface UserFormValues {
   firstName: string;
   lastName: string;
   email: string;
+  password: string;
 }
 
 const UserForm: React.FC = () => {
@@ -77,17 +18,27 @@ const UserForm: React.FC = () => {
     firstName: '',
     lastName: '',
     email: '',
+    password: '',
   };
 
   const validationSchema = Yup.object({
     firstName: Yup.string().required('First Name is required'),
     lastName: Yup.string().required('Last Name is required'),
     email: Yup.string().email('Invalid email address').required('Email is required'),
+    password: Yup.string().required('Password is required'),
   });
 
-  const handleSubmit = (values: UserFormValues) => {
-    console.log(values);
-    // You can perform further actions with the form values here
+  const handleSubmit = async (values: UserFormValues) => {
+    try {
+      const response = await axios.post('https://localhost:7124/User', values);
+      console.log(response.data); // Optional: Handle the response from the server
+      // Perform any additional actions after successful user creation
+      console.log('Registration was successful');
+    } catch (error) {
+      console.error('Error creating user:', error);
+      // Handle the error (e.g., show an error message)
+      console.log('Registration was not successful');
+    }
   };
 
   return (
@@ -116,10 +67,12 @@ const UserForm: React.FC = () => {
           </div>
           <div className="form-group">
             <label htmlFor="password">Password</label>
-            <Field type="text" id="password" name="password" className="form-control" />
+            <Field type="password" id="password" name="password" className="form-control" />
             <ErrorMessage name="password" component="div" className="error-message" />
           </div>
-          <button type="submit" className="btn btn-primary">Submit</button>
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
         </Form>
       </Formik>
     </div>
@@ -127,4 +80,5 @@ const UserForm: React.FC = () => {
 };
 
 export default UserForm;
+
 
